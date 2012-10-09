70147bfc4ff508bc0df8bf085db00bc61fdbd59c
Artem Smirnov <ac.hedgehog@gmail.com>
2012-10-09 18:19:36 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleLife::Application.config.secret_token = 'c5c1ad5e47bdbd589e5200e96e74c8bbe675b91b9b52a26b40558739b30584f877b04a44641d0e232bc40f708dfc9eb2d32065e624e46591dbf74216ac16fb8a'
