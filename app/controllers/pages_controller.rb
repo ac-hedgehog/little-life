feafff33d740615bde61fc1cf714a4b4f2b0afd1
Artem Smirnov <ac.hedgehog@gmail.com>
2012-10-15 00:19:05 +0200

    tiny calculations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,12 @@ class PagesController < ApplicationController
   end
 
   def test
-    @field = Field.new "Test Field", 50, 50
+    @field = Field.new "Test Field", 15, 15
   end
   
   def new_life
     colonies = [{ colony: Colony.new("John Dorian") }]
-    field = Field.new "Test Field", 50, 50, colonies: colonies
-    respond_to { |format| format.json { render json: field.get_life(cycles_number: 40) } }
+    field = Field.new "Test Field", 15, 15, colonies: colonies
+    respond_to { |format| format.json { render json: field.get_life(cycles_number: 20) } }
   end
 end
